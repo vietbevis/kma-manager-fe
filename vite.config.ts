33447e921b7ff859cdd9446819819b91ec7c8872
Nguyễn Văn Việt <nguyenvanviet.150204@gmail.com>
2025-06-30 22:46:36 +0700

    Add babel-plugin-react-compiler and update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+const ReactCompilerConfig = {
+  /* ... */
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]]
+      }
+    }),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
-});
+  preview: {
+    allowedHosts: ['.vittapcode.id.vn'],
+    host: '0.0.0.0',
+    port: 4173
+  }
+})
